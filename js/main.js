f51f8e1d9b1f85bed01e7b091e26c07ce870f5bc
Nikita20022-cod <grant20022@gmail.com>
2022-06-02 18:41:45 +0500

    one

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () { 
+
+      $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        }); 
+
+      $('.slider').slick({
+        dots: true,
+     }); 
+
+});
